32498308194abdf4111fb06a07e02fbbdd1ce667
Bhargav Patel <patelbhargav@me.com>
2014-07-24 10:08:34 -0400

    1st commit
    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Faith::Application.config.secret_key_base = 'cab2dd4730433a366da16f0cc754aa1e8c70ee5e8fdb8dbbe2b76544958438f14bc7ba97c0211b5f7f60b1698f3405ca83d4c20bafe9a80e80dfad13f30b1dd9'
